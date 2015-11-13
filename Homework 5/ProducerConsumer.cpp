b43af11cc44c7c4142d1efb5f906515fcaf6dbc6
Ali Bajwa <ali.z.bajwa@gmail.com>
2015-11-13 04:29:46 -0600

    Final files

--- Homework 5/ProducerConsumer.cpp
+++ Homework 5/ProducerConsumer.cpp
@@ -1,21 +1,35 @@
 /*=============================================================================
  PROGRAMMER:         Yang Wang
  COURSE:             CSC460: Operating System
- MODIFIED BY:        Ali Bajwa (ab)       __% Contribution
-                     Noah Bumgardner (nb) __% Contribution
- LAST MODIFIED DATE: 11/6/2015
+ MODIFIED BY:        Ali Bajwa (ab)       50% Contribution
+						- Coded semephores
+						- Analyzed syncronization problems
+						- Solved syncronization problems using 3 semephores
+						- Tested program repeatedly
+						- Wrote final documentation
+                     Noah Bumgardner (nb) 50% Contribution
+						- Marked critical sections
+						- Wrote pseudocode for binary semephore
+						- Wrote partial documentation
+						- Submitted hardcopy
+ LAST MODIFIED DATE: 11/13/2015
  DESCRIPTION:        A producer thread receives the count of the products n
                      through a command-line argument and creates n products
                      (simulated with n non-negative integers generated
                      randomly) for the consumer thread to consume. The two
                      threads share a bounded buffer of size bufSize.
  NOTE:               Synchronization between the two threads' operations has
-                     been inserted as commented pseudo-code. Changes are needed.
-                     Comments beginning with 'B.Sync' denote lines inserted by
-                     programmers ab and nb.
-					 Problem: We do not need two semaphores, just one binary
-                     semaphore. hw5 instructions require at least one of each
-                     kind of semaphore.
+                     been inserted. Comments ending with 'Ab' denote lines
+					 inserted by programmers ab and nb. Syncronization 
+					 problems include:
+					 1. Consumer attempting to consume before there is a
+						product in the buffer. (Counting Semephore used)
+					 2. Producer producing more products than the buffer
+						can handle. (Counting Semephore used)
+					 3. Producer and Consumer trying to add and consume
+						products simultanously. (Binary semephore used)
+					 The critical sections of both the Producer and Consumer
+					 are clearly indicated in the code using comments.
  FILES:              ProducerConsumer.cpp, showBuf.cpp, pc.h, makefile
  COMPILER:           GNU g++
  INSTRUCTION FOR COMPILATION AND EXECUTION:
@@ -27,7 +41,29 @@
 // global data shared by both producer and consumer threads
 int const bufSize = 5;
 int       buffer[bufSize];         // space to store the products
-// DECLARE 2 SEMAPHORES // B.Sync
+// DECLARE 3 SEMAPHORES - Ab
+// SRead -> Binary semephore to lock buffer read/writes - Ab
+// SProduced -> Counting semephore to prevent consumer from consuming before something is produced - Ab
+// SBuffer -> Counting semephore to prevent producer from producing if buffer is full - Ab
+int SRead = 1, SProduced = 0, SBuffer = 5; // Ab
+
+void waitBinary(int& S) {     // Wait function for Binary semephore - Ab
+	while (S == 0);
+	S = 0;
+}
+
+void signalBinary(int& S) {   // Signal function for Binary semephore - Ab
+	S = 1;
+}
+
+void waitCounting(int& S) {   // Wait function for Counting semephore - Ab
+	while (S <= 0);
+	S--;
+}
+
+void signalCounting(int& S) { // Signal function for Counting semephore - Ab
+	S++;
+}
 
 //*****************************************************************************
 void* consumerThread(void* arg);
@@ -80,12 +116,16 @@ int main(int argCount, char* argList[])
  for (int i=0; i<productCount; i++)
  {sleep(rand()%4);               // simulate the time for producing a product
   aProduct = rand()%1000;        // products are integers: 0~999
-  // Lock binary semaphore // B.Sync
+  waitCounting(SBuffer);		 // Lock Counting semephore SBuffer (prevent producing) - Ab
+  waitBinary(SRead);			 // Lock Binary semephore SRead (prevent reading) - Ab
+  // CRITICAL SECTION STARTS HERE - Ab
   buffer[in] = aProduct;         // place the product into the buffer  
   in = (in+1) % bufSize;                    
   showBuffer(Producer, buffer, bufSize, in, '#');   // '#' marks the last 
                                                     // in-product position
-  // Unlock binary semaphore // B.Sync
+  // CRITICAL SECTION ENDS HERE - Ab
+  signalCounting(SProduced);	 // Unlock Counting semephore SProduced (product exists) - Ab
+  signalBinary(SRead);			 // Unlock Binary semephore SRead (finished writing) - Ab
  }
 
 //--------------------------------------
@@ -107,12 +147,17 @@ void* consumerThread(void* arg)
      out = 0;                     // index for the next to-be-consumed product
 
  for (int i=0; i<productCount; i++)
- {// Lock binary semaphore // B.Sync
+ {
+  waitCounting(SProduced);	     // Lock Counting semephore SProduced (wait for production) - Ab
+  waitBinary(SRead);			 // Lock Binary semephore SRead (prevent writing) - Ab
+  // CRITICAL SECTION STARTS HERE - Ab
   buffer[out] = -1;              // remove a product from the buffer
   out = (out + 1) % bufSize;
   showBuffer(Consumer, buffer, bufSize, out, '*');   // '*' marks the last
                                                      // out-product position
-  // Unlock binary semaphore // B.Sync
+  // CRITICAL SECTION ENDS HERE - Ab
+  signalCounting(SBuffer);	     // Unlock Counting semephore SBuffer (space available) - Ab
+  signalBinary(SRead);			 // Unlock Binary semephore SRead (finished reading) - Ab
   sleep(rand()%4);               // simulate the time for consuming the product
  }
 
